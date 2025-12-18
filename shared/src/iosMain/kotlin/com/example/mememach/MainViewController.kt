6580527c7851482e5715c701eb31d182bf7c6ea1
arturgie <gubala-artur@wp.pl>
2025-12-18 23:05:30 +0100

    Adjust iOS part.

---
+++ shared/src/iosMain/kotlin/com/example/mememach/MainViewController.kt
@@ -0,0 +1,8 @@
+package com.example.mememach
+
+import androidx.compose.ui.window.ComposeUIViewController
+import com.example.mememach.app.App
+
+fun MainViewController() = ComposeUIViewController() {
+    App()
+}
