0b43d1a7803af1ccbf05180bbfa5a557a3ceea27
arturgie <gubala-artur@wp.pl>
2025-12-18 23:02:52 +0100

    Add entry point for app.

---
+++ shared/src/commonMain/kotlin/com/example/mememach/app/App.kt
@@ -0,0 +1,33 @@
+package com.example.mememach.app
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                color = Color.Yellow
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Hello World!"
+            )
+        }
+    }
+}
