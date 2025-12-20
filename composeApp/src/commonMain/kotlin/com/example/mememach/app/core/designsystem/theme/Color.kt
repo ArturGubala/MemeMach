5016457e45427e504009e2f61d79f6f1be5cc281
arturgie <gubala-artur@wp.pl>
2025-12-20 21:21:23 +0100

    Add theme colors.

---
+++ composeApp/src/commonMain/kotlin/com/example/mememach/app/core/designsystem/theme/Color.kt
@@ -0,0 +1,31 @@
+package com.example.mememach.app.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val Onyx = Color(0xFF0F0D13)
+internal val Mirage = Color(0xFF1D1B20)
+internal val BalticSea = Color(0xFF2B2930)
+internal val Boulder = Color(0xFF79747E)
+internal val ButterflyBush = Color(0xFF65558F)
+internal val Lavender = Color(0xFFCCC2DC)
+internal val LavenderPinocchio = Color(0xFFE6E0E9)
+internal val LavenderPink = Color(0xFFEADDFF)
+internal val LavenderMist = Color(0xFFECE6F0)
+internal val CornellRed = Color(0xFFB3261E)
+internal val CherryPie = Color(0xFF21005D)
+
+internal val LightViolet = Color(0xFFD0BCFE)
+internal val PaleLilac = Color(0xFFE0D0FA)
+internal val LightLavender = Color(0xFFAD90F1)
+
+internal val ButtonDefaultGradient =
+    listOf(
+        LavenderPink, LightViolet
+    )
+
+internal val ButtonPressedGradient = listOf(
+    PaleLilac, LightLavender
+)
+
+internal val White = Color(0xFFFFFFFF)
+internal val Black = Color(0xFF000000)
