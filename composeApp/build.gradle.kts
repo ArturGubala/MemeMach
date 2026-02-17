4a11d3f0f97ff7aa7b11b5d138677a5c96890f81
arturgie <gubala-artur@wp.pl>
2026-02-17 01:03:55 +0100

    Add dependencies with Icons.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -40,6 +40,8 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.jetbrains.compose.material.icons.core)
+            implementation(libs.jetbrains.compose.material.icons.extended)
         }
 
         androidMain.dependencies {
